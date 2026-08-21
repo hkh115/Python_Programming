ae47a5e7b90c2d08ed0b4bd34fcf610bbba033e5
hkh115 <hkh100115@gmail.com>
2026-08-21 14:20:04 +0900

    datatype files

---
+++ 01_datatype/ex1.py
@@ -0,0 +1,39 @@
+a = 2
+b = 3
+print(a, end = "")
+print(b)
+print(a, b, sep=",")
+
+a = 2, b
+print(a)
+print(type(a))
+
+a = 2; b = 3
+print(a, b)
+
+x = y = z = 0
+
+a, b = 2, 3
+print(a, b)
+
+temp = a
+a = b
+b = temp
+
+print(a, b)
+print(a, b)
+
+a, b = b, a
+print(a, b)
+
+name2 = "pororo"
+_name = "pororo"
+# class = "test"
+# name! = "test"
+
+이름 = "뽀로로"
+print(이름)
+
+student_name = "크롱"
+studentName = "크롱"
+MAX_COUNT = 100
---
+++ 01_datatype/ex3.py
@@ -0,0 +1,19 @@
+a = True
+print(a, type(a))
+
+print(1 < 0)
+print(1 > 0)
+print(1 == 0)
+print(1 != 0)
+
+print("apple" > "banana")
+
+print(bool(3))
+print(bool(0))
+print(bool("hello"))
+print(bool(""))
+print(bool([10]))
+print(bool([]))
+
+a = None
+print(a, type(a))
